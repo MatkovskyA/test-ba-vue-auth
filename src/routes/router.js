e61ce711d276226dd4d2912564d1c3895407f00b
MatkovskyA <a.matkovsky14@gmail.com>
2024-11-15 20:18:09 +0300

    Fix deploy

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import AccountPage from "../components/AccountPage.vue";
 import HelloWorld from "../components/HelloWorld.vue";
 import LoginPage from "../components/LoginPage.vue";
 
 export default createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
     {
     path: '/dashboard',
