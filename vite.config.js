acaf26a5f73e345192f4a80213fa259930c4d924
MatkovskyA <a.matkovsky14@gmail.com>
2024-11-15 20:11:30 +0300

    Fix deploy

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,17 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/test-ba-vue-auth/',
+// export default defineConfig({
+//   base: '/test-ba-vue-auth/',
+//   plugins: [vue()],
+// })
+
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "/test-ba-vue-auth/" : "/",
   plugins: [vue()],
-})
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+}));
